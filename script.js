83168d6ce9a9a6f18f34762db20ea0f5addbfced
smdewi <smdewi2020@gmail.com>
2022-01-18 15:41:37 +0800

    move out the start button to landing page

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 /** Video Poker */
 
-// Create 
-const pokerTableEl = document.createElement('div');
-pokerTableEl.classList.add('poker-table');
-document.body.appendChild(pokerTableEl);
+/** This function creates the initial landing page */
+const buildLandingPage = () => {
+  // Create poker table element that will hold all other game elements
+  const pokerTableEl = document.createElement('div');
+  pokerTableEl.classList.add('poker-table');
+  pokerTableEl.innerText = "Welcome to Video Poker";
+  document.body.appendChild(pokerTableEl);
+
+  //create start button to start the game
+  const startButton = document.createElement('button');
+  startButton.classList.add('start-button');
+  startButton.innerText = "Start";
+  pokerTableEl.appendChild(startButton);
+}
+
+const initGame = () => {
+  buildLandingPage();
+
+  
+};
+
+initGame();
