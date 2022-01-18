ea4637f6004c1a7645a70dc0067606b73a86102c
smdewi <smdewi2020@gmail.com>
2022-01-18 16:22:46 +0800

    start over

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 /** Video Poker */
 
-/** This function creates the initial landing page */
-const buildLandingPage = () => {
-  // Create poker table element that will hold all other game elements
-  const pokerTableEl = document.createElement('div');
-  pokerTableEl.classList.add('poker-table');
-  pokerTableEl.innerText = "Welcome to Video Poker";
-  document.body.appendChild(pokerTableEl);
+/** Global variables */
 
-  //create start button to start the game
-  const startButton = document.createElement('button');
-  startButton.classList.add('start-button');
-  startButton.innerText = "Start";
-  pokerTableEl.appendChild(startButton);
-}
+
+
 
 const initGame = () => {
-  buildLandingPage();
-
-  
+ 
 };
 
 initGame();
