19ef9fe249295d5bceb2362c2bd46d25f42814f8
smdewi <smdewi2020@gmail.com>
2022-01-18 20:42:30 +0800

    divide pokerTable into 3: top, middle, bottom

--- script.js
+++ script.js
@@ -18,6 +18,24 @@ const initGame = () => {
   playerHandEl.classList.add('player-hand');
   pokerTableEl.appendChild(playerHandEl);
 
+  // Create card element where the player's cards will be shown
+  // Use a loop to create 5 card elements
+  for (let i = 0; i < maxCard; i += 1) {
+    const cardEl = document.createElement('div');
+    cardEl.classList.add('cardEl');
+    playerHandEl.appendChild(cardEl);
+  }
+  
+  // Create a section for where the deal button and timer will appear
+  const middleTable = document.createElement('div');
+  middleTable.classList.add('middle-table');
+  pokerTableEl.appendChild(middleTable);
+
+  // Create a section for where the place bet and display message will appear
+  const bottomTable = document.createElement('div');
+  bottomTable.classList.add('bottom-table');
+  pokerTableEl.appendChild(bottomTable);
+
 };
 
 initGame();
