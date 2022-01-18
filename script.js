9ad1112ebc70a9a68d66be9f98e1ba77be09050b
smdewi <smdewi2020@gmail.com>
2022-01-18 16:48:06 +0800

    create container where player's cards will show up

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 /** Video Poker */
 
 /** Global variables */
-
+// The maximum number of cards a player can have is 5
+const maxCard = 5;
 
 
 
 const initGame = () => {
  
+  // Create poker table elements where all game elements will show up
+  const pokerTableEl = document.createElement('div');
+  pokerTableEl.classList.add('poker-table');
+  document.body.appendChild(pokerTableEl);
+
+  // The top half of the poker table element will show the player's cards
+  const playerHandEl = document.createElement('div');
+  playerHandEl.classList.add('player-hand');
+  pokerTableEl.appendChild(playerHandEl);
+
 };
 
 initGame();
