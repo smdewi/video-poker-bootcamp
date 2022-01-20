c8358b6b9aefd02c32cc78c4d9fb610057d866a7
smdewi <smdewi2020@gmail.com>
2022-01-20 09:11:42 +0800

    create a playerHand to test score calculation

---
+++ testHandscript.js
@@ -0,0 +1,9 @@
+// Store player's hand in an array
+const playerHand = [
+  {rank: 2, suit: '❤', name: '2'},
+  {rank: 2, suit: '♦', name: '2'},
+  {rank: 5, suit: '♣', name: '5'},
+  {rank: 7, suit: '♠', name: '7'},
+  {rank: 9, suit: '♠', name: '9'},
+];
+
