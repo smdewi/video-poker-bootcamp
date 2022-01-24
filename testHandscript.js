60c81cb3ef47626d7a3eaea3df2befcee3110224
smdewi <smdewi2020@gmail.com>
2022-01-24 22:36:57 +0800

    testplayerHand and calculateHand score check

--- testHandscript.js
+++ testHandscript.js
@@ -1,9 +1,32 @@
 // Store player's hand in an array
-const playerHand = [
+const testPlayerHand = [
   {rank: 2, suit: '❤', name: '2'},
-  {rank: 2, suit: '♦', name: '2'},
-  {rank: 5, suit: '♣', name: '5'},
-  {rank: 7, suit: '♠', name: '7'},
-  {rank: 9, suit: '♠', name: '9'},
+  {rank: 3, suit: '❤', name: '3'},
+  {rank: 4, suit: '❤', name: '4'},
+  {rank: 5, suit: '❤', name: '5'},
+  {rank: 6, suit: '❤', name: '6'},
 ];
+
+let isFlush = false;
+let playerHandPoints = 0;
+
+const checkForFlush = (playerHand) => {
+  let sameSuit = 0;
+  // check if playerHand.suit are all the same
+  for (let i = 0; i < 4; i += 1){
+    if (playerHand[i].suit === playerHand[i+1].suit) {
+      sameSuit += 1;
+    }
+  }
+  
+  if (sameSuit === 4) {
+    isFlush = true;
+    playerHandPoints = 20;
+  }
+  else {
+    isFlush = false;
+  }
+
+  return isFlush;
+};
 
