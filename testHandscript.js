57f93d75b8fd285f29a25081c058e743b29e83f8
smdewi <smdewi2020@gmail.com>
2022-01-25 12:45:30 +0800

    create straightFlushHand for testing

--- testHandscript.js
+++ testHandscript.js
@@ -1,32 +1,10 @@
 // Store player's hand in an array
-const testPlayerHand = [
-  {rank: 2, suit: '❤', name: '2'},
-  {rank: 3, suit: '❤', name: '3'},
-  {rank: 4, suit: '❤', name: '4'},
-  {rank: 5, suit: '❤', name: '5'},
-  {rank: 6, suit: '❤', name: '6'},
+const straightFlushHand = [
+  {name: '3', suit: 'hearts', suitSymbol: '❤', rank: 3, color: 'red',},
+  {name: '2', suit: 'hearts', suitSymbol: '❤', rank: 2, color: 'red',},
+  {name: '5', suit: 'hearts', suitSymbol: '❤', rank: 5, color: 'red',},
+  {name: '4', suit: 'hearts', suitSymbol: '❤', rank: 4, color: 'red',},
+  {name: 'A', suit: 'hearts', suitSymbol: '❤', rank: 1, color: 'red',},
 ];
 
-let isFlush = false;
-let playerHandPoints = 0;
-
-const checkForFlush = (playerHand) => {
-  let sameSuit = 0;
-  // check if playerHand.suit are all the same
-  for (let i = 0; i < 4; i += 1){
-    if (playerHand[i].suit === playerHand[i+1].suit) {
-      sameSuit += 1;
-    }
-  }
-  
-  if (sameSuit === 4) {
-    isFlush = true;
-    playerHandPoints = 20;
-  }
-  else {
-    isFlush = false;
-  }
-
-  return isFlush;
-};
 
